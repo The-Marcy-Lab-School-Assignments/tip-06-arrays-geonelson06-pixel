59232025fc797de4951ea350986d151a3bb65181
Gabriel <geonelson06@gmail.com>
2025-10-08 11:03:20 -0400

    Learned arrays

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+const findMax = (numbers) => {
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+}
+console.log(findMax([5, 3, 8, 9, 15]));
